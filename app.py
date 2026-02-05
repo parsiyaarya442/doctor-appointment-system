377d5b09de3521cd25fac900a2fd8ee77976b20e
PARSIYAARYA <parsiyaarya442@gmail.com>
2026-02-05 16:33:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,10 @@ from flask import Flask, render_template, request, redirect, session
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = "secret123"
 @app.route("/")
 def login():
     return render_template("login.html")
-if __name__ =="__main__":
-app.run()
-app.secret_key = "secret123"
-
 def get_db():
     return sqlite3.connect("database.db")
 
